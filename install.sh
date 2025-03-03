877b786a94831967ba7dc4c06d600f1adb4c032a
ajuvi <102821845+ajuvi@users.noreply.github.com>
2025-03-03 15:48:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+echo""
+echo "INSTAL·LADOR BIND9"
+echo "------------------"
+
 if ! command -v named &> /dev/null; then
     echo "Instal·lant el bind9..."
     sudo apt-get update
     sudo apt-get install -y bind9 dnsutils
+else
+    echo "Instal·lant el bind9..."    
 fi
 
 echo "Copiant els fitxers de configuració..."
@@ -14,8 +20,8 @@ sudo cp ./db.asix.local /etc/bind/db.asix.local
 sudo chown -R bind:bind /etc/bind
 
 echo "Reiniciant el bind9..."
-sudo systemctl restart bind9
-sudo systemctl enable bind9
+sudo systemctl restart named
+sudo systemctl enable named
 
 echo "Estat del bind9..."
-sudo systemctl status bind9
+sudo systemctl status named
